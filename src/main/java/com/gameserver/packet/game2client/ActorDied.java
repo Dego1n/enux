5d61e9b54eacdfee77c7c8a5bc506b71ea345feb
Alexander Ogurtsov <mail@dego1n.ru>
2019-11-02 18:23:19 +0300

    Добавлена заглушка в ActorDied со списком лута

--- src/main/java/com/gameserver/packet/game2client/ActorDied.java
+++ src/main/java/com/gameserver/packet/game2client/ActorDied.java
@@ -5,6 +5,8 @@ import com.gameserver.packet.AbstractSendablePacket;
 import com.gameserver.packet.IServerPacket;
 import com.gameserver.packet.ServerPackets;
 
+import java.util.Random;
+
 public class ActorDied extends AbstractSendablePacket implements IServerPacket {
 
     private BaseActor actor;
@@ -20,5 +22,17 @@ public class ActorDied extends AbstractSendablePacket implements IServerPacket {
         writeH(ServerPackets.ACTOR_DIED);
         writeD(actor.getObjectId());
         writeD(10); //Монтаж смерти -> через сколько вызвать Destroy актора
+        Random rnd = new Random();
+        int loot_count = rnd.nextInt(5);
+        writeH(loot_count);
+        System.out.println("Sending loot count: "+loot_count);
+        for(int i = 0; i<loot_count; i++)
+        {
+            writeD(-1); //object_id TODO:
+            Random rnd2 = new Random();
+            writeD(rnd2.nextInt(2)+1);//item _id
+            writeD(1); //count
+
+        }
     }
 }
