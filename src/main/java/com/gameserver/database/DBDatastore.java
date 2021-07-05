f9b93bf55e73cf1708eb9504223761d7247182a6
Dego1n <mail@dego1n.ru>
2021-07-05 13:27:31 +0300

    Remove PostgreSQL from Config and add new MongoDB configuration settings

--- src/main/java/com/gameserver/database/DBDatastore.java
+++ src/main/java/com/gameserver/database/DBDatastore.java
@@ -1,5 +1,6 @@
 package com.gameserver.database;
 
+import com.gameserver.config.Config;
 import com.gameserver.database.entity.actor.Character;
 import com.mongodb.client.MongoClients;
 import dev.morphia.Datastore;
@@ -9,7 +10,10 @@ public class DBDatastore {
     private static Datastore _datastore = null;
     public static Datastore getInstance() {
         if(_datastore == null) {
-            _datastore = Morphia.createDatastore(MongoClients.create(), "enux");
+            _datastore = Morphia.createDatastore(
+                    MongoClients.create(Config.DATABASE_CONNECTION_STRING),
+                    Config.DATABASE_NAME
+            );
             _datastore.getMapper()
                     .mapPackageFromClass(Character.class);
             _datastore.ensureIndexes();
